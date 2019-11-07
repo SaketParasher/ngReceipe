254f828311b21f8d65d98a9e1677eaad23d1b678
SaketParasher <saket.parasher@gmail.com>
2019-11-08 00:09:11 +0530

    second commit

--- src/app/receipe/receipe-list/receipe-list.component.ts
+++ src/app/receipe/receipe-list/receipe-list.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+import { Receipe } from "../receipe.model";
 
 @Component({
-  selector: 'app-receipe-list',
-  templateUrl: './receipe-list.component.html',
-  styleUrls: ['./receipe-list.component.css']
+  selector: "app-receipe-list",
+  templateUrl: "./receipe-list.component.html",
+  styleUrls: ["./receipe-list.component.css"]
 })
 export class ReceipeListComponent implements OnInit {
+  receipes: Receipe[] = [
+    new Receipe(
+      "A Test Teceipe",
+      "A Unique Receipe with unique Test",
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/best-stuffing-recipe-1570197599.jpg?crop=1.00xw:0.669xh;0,0.296xh&resize=480:*"
+    )
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- src/app/shoppingl-list/shoppingl-list.component.ts
+++ src/app/shoppingl-list/shoppingl-list.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Ingrediant } from "../shared/ingrediant.model";
 
 @Component({
-  selector: 'app-shoppingl-list',
-  templateUrl: './shoppingl-list.component.html',
-  styleUrls: ['./shoppingl-list.component.css']
+  selector: "app-shoppingl-list",
+  templateUrl: "./shoppingl-list.component.html",
+  styleUrls: ["./shoppingl-list.component.css"]
 })
 export class ShoppinglListComponent implements OnInit {
+  ingrediants: Ingrediant[] = [
+    new Ingrediant("Apples", 5),
+    new Ingrediant("Tomato", 10)
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
